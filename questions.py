d59a40dc1ebce3fe50ce1dd9fa40888590debabd
dondiosito <francoalconada@gmail.com>
2025-03-20 14:18:59 -0300

    correccion bugs por ingresos de not int

--- questions.py
+++ questions.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 # Preguntas para el juego
 questions = [
@@ -36,6 +37,19 @@ for _ in range(3):
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
+
+        user_input = input("Respuesta: ")
+
+        if not user_input.isdigit():  # Verifica si no es un número
+            print("Respuesta no válida")
+            sys.exit(1)  # Termina el programa con estado de error 1
+
+        user_answer = int(user_input) - 1  # Convertir entrada a índice de lista
+
+        if user_answer < 0 or user_answer >= len(answers[question_index]):
+            print("Respuesta no válida")
+            sys.exit(1)
+
         user_answer = int(input("Respuesta: ")) - 1
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
@@ -54,13 +68,16 @@ for _ in range(3):
 5. Crea tu propio repositorio **remoto** en [Github](https://github.com/) y suba el archivo al repositorio remoto.
 6. Agrega el `README.md` con tu nombre y número de estudiante.
 7. Modifique el programa anterior con las siguientes funcionalidades:
+
 	- El juego tiene un bug. Si el usuario ingresa un número de respuesta no válido por ejemplo 42 o 
     "huevos con spam" el programa falla con un error. Modifica el mismo para que si la respuesta no
     es un número o bien no está en el rango de las respuestas posibles muestre un mensaje diciendo: 
     "Respuesta no válida" y termine de inmediato con exit status igual a 1. 
+
 	- Modifique el juego para que al final de la partida se muestre el puntaje del jugador o
     jugadora. El puntaje se calcula de la siguiente forma, cada intento fallido descuenta 0.5 puntos
     y cada acierto suma 1 punto.
+    
     - Modifique el juego para, en lo posible, no acceder a las 3 listas usando índices. Ayuda:
     ```python
     questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
